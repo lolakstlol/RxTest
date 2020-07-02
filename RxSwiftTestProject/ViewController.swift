52f81be6d3f4a43f651d61d80b1763bf60b2efe9
a.kholodok <a.kholodok@bsl.dev>
2020-07-02 13:10:55 +0300

    Initial Commit

---
+++ RxSwiftTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftTestProject
+//
+//  Created by Артем Холодок on 02.07.2020.
+//  Copyright © 2020 Артем Холодок. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
